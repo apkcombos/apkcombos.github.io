c84832b329dd3f6924a561dd116252b71dd56c1f
brutefaces <cabem257@gmail.com>
2025-10-26 09:28:19 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Categories from "./pages/Categories";
+import CategoryDetail from "./pages/CategoryDetail";
+import AppDetail from "./pages/AppDetail";
+import Search from "./pages/Search";
+import Download from "./pages/Download";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/category/:slug" element={<CategoryDetail />} />
+          <Route path="/app/:id" element={<AppDetail />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/download" element={<Download />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
